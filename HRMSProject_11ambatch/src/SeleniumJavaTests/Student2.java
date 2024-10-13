9ce1c50cf077e46d1f0ad8e453cb801acb526573
GitDemo <10am.seleniumjavabatch@gmail.com>
2024-10-13 14:47:34 +0530

    changes added to Hanumanth_feature

---
+++ HRMSProject_11ambatch/src/SeleniumJavaTests/Student2.java
@@ -0,0 +1,21 @@
+package SeleniumJavaTests;
+
+public class Student2 {
+	
+	public static void main(String[] args) {
+
+		//Local variables
+		
+		//private String name="sai";  //invalid
+		public double d=10.5; //invalid
+		protected boolean b=false;  //invalid
+		
+		      int x =10;//valid
+		final int y=15; //valid
+		
+		final String name = "sai";
+		
+		
+	}
+
+}
